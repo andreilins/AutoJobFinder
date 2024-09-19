4a1185af2b79ae9c0833f17e8e0392b30504e014
andreilins <a.l1n5@protonmail.ch>
2024-09-19 17:20:07 +0100

    notherdaynotherdoller

--- JobScraper/main.py
+++ JobScraper/main.py
@@ -1,25 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.common.by import By
-import time
 
-# Step 1: Set up the WebDriver (this will automatically install the appropriate GeckoDriver)
+# Set up WebDriver
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
-# Step 2: Navigate to a website (e.g., Google)
-driver.get("https://www.google.com")
-
-# Step 3: Print the page title
-print("Page title is:", driver.title)
-
-# Optional: Find an element (Google search bar) and interact with it
-search_box = driver.find_element(By.NAME, "q")
-search_box.send_keys("Hello World!")
-search_box.submit()  # Submit the search form
-
-# Optional: Wait for a while to see the results
-time.sleep(5)
-
-# Close the browser after executing the commands
-driver.quit()
+# Navigate to the login page
+driver.get("https://www.linkedin.com/login")
